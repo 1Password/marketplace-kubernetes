04bcd6d14c3770b7d5007f2c0379bd16f60ead93
De Ville Weppenaar <dweppenaar@gmail.com>
2021-04-07 15:13:52 -0700

    Revert "Convert Netdata to Helm deploy.sh (#199)"
    This reverts commit 985d363edd4500911a602b1e89a21475334705f2.

--- stacks/netdata/deploy-local.sh
+++ stacks/netdata/deploy-local.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
 # create namespace
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
@@ -14,7 +16,7 @@ EOF
 kubectl config set-context --current --namespace=netdata
 
 # deploy netdata
-kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/netdata/yaml/netdata.yaml
+kubectl apply -f "$ROOT_DIR"/stacks/netdata/yaml/netdata.yaml
 
 # ensure services are running
 kubectl rollout status -w statefulset/netdata-master
